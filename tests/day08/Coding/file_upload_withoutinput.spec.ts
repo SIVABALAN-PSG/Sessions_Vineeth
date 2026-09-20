3c80e7d00cbd4b38438d499c03ba0c5618609c25
C SIVABALAN <balulingam007@gmail.com>
2026-09-20 17:54:29 +0530

    updated node and resolved error for path import

--- tests/day08/Coding/file_upload_withoutinput.spec.ts
+++ tests/day08/Coding/file_upload_withoutinput.spec.ts
@@ -2,24 +2,24 @@ import { test } from "@playwright/test";
 import path from "path";
 
 test("Test to verify file upload", async ({ page }) => {
+  // Load the application URL.
+  await page.goto("https://blazorise.com/docs/components/file-picker");
 
-    // Load the application URL.
-    await page.goto("https://blazorise.com/docs/components/file-picker");
+  // Register an event listener for the file chooser event.
+  const fileUploadPromise = page.waitForEvent("filechooser");
 
-    // Register an event listener for the file chooser event.
-    const fileUploadPromise = page.waitForEvent("filechooser");
+  // Click the Choose files upload element.
+  await page.locator("//span[text()='Choose files']").first().click();
 
-    // Click the Choose files upload element.
-    await page.locator("//span[text()='Choose files']").first().click();
+  // Wait for the file chooser event and get the file chooser reference.
+  const fileUploadRef = await fileUploadPromise;
 
-    // Wait for the file chooser event and get the file chooser reference.
-    const fileUploadRef = await fileUploadPromise;
+  // Set the file to be uploaded.
 
-    // Set the file to be uploaded.
-    await fileUploadRef.setFiles(
-        path.join(__dirname, "../../Data/TestleafLogo.png")
-    );
+  (await fileUploadPromise).setFiles(
+    path.join(__dirname, "../../../Data/Testleaf.png"),
+  );
 
-    // Wait for 5 seconds after uploading the file.
-    await page.waitForTimeout(5000);
+  // Wait for 5 seconds after uploading the file.
+  await page.waitForTimeout(5000);
 });
